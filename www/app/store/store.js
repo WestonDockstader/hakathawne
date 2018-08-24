6c74f8a6d9597568d7e51a763600297a87453119
WestonDockstader <dockstaderweston@gmail.com>
2018-08-24 09:49:30 -0600

    initial commit

---
+++ www/app/store/store.js
@@ -0,0 +1,42 @@
+import User from '../models/user.js'
+
+let store
+
+let state = {
+  user: {}
+}
+
+function setState(prop, data) {
+  state[prop] = data
+  console.log(state)
+}
+
+export default class Store {
+  login(creds, draw) {
+    fetch('/auth/login', {
+      method: 'post',
+      body: JSON.stringify(creds),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        setState('user', new User(data))
+        draw()
+      })
+      .catch(console.error)
+  }
+
+  constructor() {
+    if (store) {
+      return store
+    }
+    store = this
+  }
+  get state() {
+    return {
+      ...state
+    }
+  }
+}
